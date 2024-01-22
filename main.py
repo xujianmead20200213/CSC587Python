2e67177513c4764f4b8d3f1f204545ce2b5cd6f4
xujianmead20200213 <471103891@qq.com>
2024-01-22 00:34:46 -0500

    Test start

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+#
+def get_average(values):
+    result = 0
+    for v in values:
+        result += v
+    return result / len(values)
+
+
+print("Average", get_average([5, 6, 7, 8]))
+
+
+
